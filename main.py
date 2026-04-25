d26c1ccb815cf67f143c7a34b2052370ee79c0cd
Fabric Engineer <msfabricengineer@gmail.com>
2026-04-25 13:38:55 +0100

    Added subtract functionality in main.py and calculator.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from utils.calculator import add_nums
+from utils.calculator import add_nums, subtract_nums
 
-if __name__ == "__name__":
+if __name__ == "__main__":
 
     print(add_nums(2,3))
+    print(subtract_nums(2,3))
