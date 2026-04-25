04594a6b817d01c726fc19cb6feb8c12ba4e90d7
Fabric Engineer <msfabricengineer@gmail.com>
2026-04-25 13:33:04 +0100

    Added multiply functionality in main.py and calculator.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from utils.calculator import add_nums
+from utils.calculator import add_nums, multiply_nums
 
 if __name__ == "__name__":
 
     print(add_nums(2,3))
+    print(multiply_nums(2,3))
--- utils/calculator.py
+++ utils/calculator.py
@@ -1,2 +1,5 @@
 def add_nums(a,b):
     return a+b
+
+def multiply_nums(a,b):
+    return a*b
